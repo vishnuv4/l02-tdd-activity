a610bb7b8439a7ece3213149811d59797fee3ea7
Vishnu Venkatesh <vishnuv@seas.upenn.edu>
2024-09-11 18:57:30 +0000

    initial

--- src/multiplexer/inc/multiplexer.h
+++ src/multiplexer/inc/multiplexer.h
@@ -2,7 +2,10 @@
 #define MULTIPLEXER_H
 
 // Any libraries needed here
+#include "stdbool.h"
 
 // Add the functions needed for your multiplexer functions
+
+bool mux(bool *inputs, int select);
 
 #endif
--- src/multiplexer/test/test_multiplexer.c
+++ src/multiplexer/test/test_multiplexer.c
@@ -1,8 +1,11 @@
 #include <multiplexer.h>
 #include <stdint.h>
 #include <unity.h>
+#include "stdbool.h"
 // See
 // https://github.com/ThrowTheSwitch/Unity/blob/master/docs/UnityGettingStartedGuide.md
+
+bool inputArr[8] = {1,1,0,0,1,0,1,0};
 
 void setUp(void) {
   // Set stuff up here
@@ -13,8 +16,8 @@ void tearDown(void) {
 }
 
 /* Your tests here! */
-void test_sampleSucceed(void) { TEST_ASSERT_EQUAL(1, 1); }
+// void test_sampleSucceed(void) { TEST_ASSERT_EQUAL(1, 1); }
 
-void test_sampleFail(void) { TEST_ASSERT_EQUAL(1, 2); }
+// void test_sampleFail(void) { TEST_ASSERT_EQUAL(1, 2); }
 
-void test_sampleIgnored(void) { TEST_IGNORE(); }
+// void test_sampleIgnored(void) { TEST_IGNORE(); }
