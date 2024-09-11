2c3a569f79940baabc462f46ebdb178a5e83abe3
Vishnu Venkatesh <vishnuv@seas.upenn.edu>
2024-09-11 20:47:52 +0000

    asdfg

--- src/multiplexer/src/multiplexer.c
+++ src/multiplexer/src/multiplexer.c
@@ -12,8 +12,8 @@ void mux_init(void)
 void mux_setOutput(int num)
 {
     muxHandler_g.A[0] = num & (1 << 0);
-    muxHandler_g.A[1] = num & (1 << 1);
-    muxHandler_g.A[2] = num & (1 << 2);
+    muxHandler_g.A[1] = (num & (1 << 1)) >> 1;
+    muxHandler_g.A[2] = (num & (1 << 2)) >> 2;
 }
 
 bool mux_getOutput(void)
