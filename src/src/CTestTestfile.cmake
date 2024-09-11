51c4a7499cfeb8625f6724e160de3eb8fe96f2f2
Nick McGill-Gardner <nmcgill@upenn.edu>
2024-09-11 00:41:15 -0400

    added gitignore and fixed all the goofy things

---
+++ src/src/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/nmcgill/github/5180/l02-tdd-activity/src
+# Build directory: /Users/nmcgill/github/5180/l02-tdd-activity/src/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("multiplexer")
---
+++ src/src/multiplexer/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/nmcgill/github/5180/l02-tdd-activity/src/multiplexer
+# Build directory: /Users/nmcgill/github/5180/l02-tdd-activity/src/src/multiplexer
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_multiplexer "/Users/nmcgill/github/5180/l02-tdd-activity/src/src/multiplexer/test_multiplexer")
+set_tests_properties(test_multiplexer PROPERTIES  _BACKTRACE_TRIPLES "/Users/nmcgill/github/5180/l02-tdd-activity/CMakeLists.txt;47;add_test;/Users/nmcgill/github/5180/l02-tdd-activity/src/multiplexer/CMakeLists.txt;6;add_unit_test;/Users/nmcgill/github/5180/l02-tdd-activity/src/multiplexer/CMakeLists.txt;0;")
